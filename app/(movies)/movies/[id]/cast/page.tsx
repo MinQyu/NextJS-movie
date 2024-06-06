5cac3a9d52454bc8b19029c836340c84871e1ea3
MinQyu <jorushs@gmail.com>
2024-06-06 19:42:29 +0900

    Cast페이지 메타데이터 수정

--- app/(movies)/movies/[id]/cast/page.tsx
+++ app/(movies)/movies/[id]/cast/page.tsx
@@ -1,6 +1,10 @@
 import MovieCast from "../../../../../components/movie-cast";
 import { IParams } from "../page";
 
+export const metadata = {
+  title: "Cast",
+};
+
 export default function Cast({ params: { id } }: IParams) {
   return <MovieCast id={id} />;
 }
